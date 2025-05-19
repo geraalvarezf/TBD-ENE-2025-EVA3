4e20a4950871293587025ac8536c799e6971ac2a
luis alvarez <126295031+geraalvarezf@users.noreply.github.com>
2025-05-19 16:50:30 -0600

    EVA3_5_PROCEDIMIENTOS.txt

---
+++ EVA3_5_PROCEDIMIENTOS_4.sql
@@ -0,0 +1,7 @@
+delimiter $$
+create procedure insertar_cliente(in id_cl int, in f_n varchar(45), l_n varchar(45))
+begin
+	insert into clientes(id_cliente, nombre, apellido)
+    values(id_cl, f_n, l_n);
+end $$
+delimiter ;
